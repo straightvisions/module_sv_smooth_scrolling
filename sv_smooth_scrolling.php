6b680658da1632d2e8483064278c0d8764dea2d6
Matthias Reuter <info@straightvisions.com>
2018-10-24 08:12:29 +0200

    init

---
+++ sv_smooth_scrolling.php
@@ -0,0 +1,23 @@
+<?php
+	namespace sv_100;
+	
+	/**
+	 * @author			Matthias Reuter
+	 * @package			sv_100
+	 * @copyright		2017 Matthias Reuter
+	 * @link			https://straightvisions.com
+	 * @since			1.0
+	 * @license			See license.txt or https://straightvisions.com
+	 */
+	class sv_smooth_scrolling extends init{
+		static $scripts_loaded						= false;
+
+		public function __construct($path,$url){
+			$this->path								= $path;
+			$this->url								= $url;
+			$this->name								= get_class($this);
+			
+			$this->module_enqueue_scripts(false);
+		}
+	}
+?>
